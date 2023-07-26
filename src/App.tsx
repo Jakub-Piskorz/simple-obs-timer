6093d55faee79e049e0619b50fa318703c78382c
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2023-07-27 01:16:54 +0200

    removed ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 import useCssParser from './utils/use-css-parser';
 import DameDaNe from './assets/dame.mp3';
